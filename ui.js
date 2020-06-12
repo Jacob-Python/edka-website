75a8e59e4ce7e4d978a3074dfaa0949c8c06df0b
Jacob-Python <58337545+Jacob-Python@users.noreply.github.com>
2020-06-12 12:29:47 -0400

    add slides

--- ui.js
+++ ui.js
@@ -1,3 +1,4 @@
+let i = 1;
 function showdiv(id){
 	if (document.getElementById(id).hidden){
 		document.getElementById(id).hidden = false;
@@ -6,4 +7,30 @@ function showdiv(id){
 		document.getElementById(id).hidden = true;
 		document.getElementById(id+"a").innerHTML = document.getElementById(id+"a").innerHTML.replace("-","+")
 	}
+}
+function next_img(){
+	i ++;
+	if (i > 2){
+		i = 1;
+		document.getElementById('1').setAttribute("fill", "grey");
+		document.getElementById('2').setAttribute("fill", "black");
+	} else {
+		document.getElementById('2').setAttribute("fill", "grey");
+		document.getElementById('1').setAttribute("fill", "black");
+	}
+	document.getElementById("slide").src = i+".jpg"
+}
+function last_img(){
+	i --;
+	if (i < 1){
+		i = 2;
+		document.getElementById('2').setAttribute("fill", "grey");
+		document.getElementById('1').setAttribute("fill", "black");
+	} else {
+		document.getElementById('1').setAttribute("fill", "grey");
+		document.getElementById('2').setAttribute("fill", "black");
+	}
+	document.getElementById("slide").src = i+".jpg"
+	document.getElementById(i).fill = "grey"
+	document.getElementById(i+1).fill = "black"
 }
